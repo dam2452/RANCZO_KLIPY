3ff0aef97496add488333b186a4e195eca5d6aab
dam2452 <zodiaczek@gmail.com>
2024-03-28 18:39:11 +0100

    www

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Użyj oficjalnego obrazu Pythona jako obrazu bazowego
-FROM python:3.9
+FROM python:3.12
 
 # Zaktualizuj listę pakietów i zainstaluj git
 RUN apt-get update && apt-get install -y git
--- indexer.py
+++ indexer.py
@@ -1,45 +1,45 @@
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 from elasticsearch import Elasticsearch, helpers
 import json
 import os
 import git
+from dotenv import load_dotenv
 
-# Użyj zmiennej środowiskowej dla adresu URL repozytorium
-#REPO_URL = os.environ.get("REPO_URL")
+# Załaduj zmienne środowiskowe z pliku .env
+load_dotenv("passwords.env")
 
-es = Elasticsearch(["http://192.168.0.210:30003"])
+es_host = os.getenv("ES_HOST")
+es_username = os.getenv("ES_USERNAME")
+es_password = os.getenv("ES_PASSWORD")
+telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# def clone_or_update_repo(repo_url, local_path="Ranczo-Transkrypcje"):
-#     if os.path.exists(local_path):
-#         print("Aktualizowanie repozytorium...")
-#         repo = git.Repo(local_path)
-#         origin = repo.remotes.origin
-#         origin.pull()
-#     else:
-#         print("Klonowanie repozytorium...")
-#         git.Repo.clone_from(repo_url, local_path)
+es = Elasticsearch(
+    [es_host],
+    basic_auth=(es_username, es_password),
+    verify_certs=False,
+)
 
-def index_transcriptions(base_path="Ranczo-Transkrypcje"):
+def index_transcriptions(base_path="RANCZO-TRANSKRYPCJE"):
     actions = []
     for season_dir in os.listdir(base_path):
         season_path = os.path.join(base_path, season_dir)
-        for episode_file in os.listdir(season_path):
-            if episode_file.endswith(".json"):
-                episode_video_path = episode_file.replace(".json", ".mp4")
-                file_path = os.path.join(season_path, episode_file)
-                with open(file_path, 'r') as file:
-                    transcription = json.load(file)
-                    for segment in transcription["segments"]:
-                        segment["video_path"] = os.path.join("Ranczo-Wideo", season_dir, episode_video_path)
-                        action = {
-                            "_index": "ranczo-transcriptions",
-                            "_source": segment
-                        }
-                        actions.append(action)
+        if os.path.isdir(season_path):  # Dodaj tę linię
+            for episode_file in os.listdir(season_path):
+                if episode_file.endswith(".json"):
+                    episode_video_path = episode_file.replace(".json", ".mp4")
+                    file_path = os.path.join(season_path, episode_file)
+                    with open(file_path, 'r') as file:
+                        transcription = json.load(file)
+                        for segment in transcription["segments"]:
+                            segment["video_path"] = os.path.join("RANCZO-WIDEO", season_dir, episode_video_path)
+                            action = {
+                                "_index": "ranczo-transcriptions",
+                                "_source": segment
+                            }
+                            actions.append(action)
     helpers.bulk(es, actions)
 
 if __name__ == "__main__":
-    # if REPO_URL is None:
-    #     print("Nie znaleziono URL repozytorium. Ustaw zmienną środowiskową REPO_URL.")
-    # else:
-    #     clone_or_update_repo(REPO_URL)
         index_transcriptions()
