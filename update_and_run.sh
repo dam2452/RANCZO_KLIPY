3e5f17f69d6efe8e7ef9c445164bb0ddeed6c92a
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-03-30 13:19:26 +0100

    .

--- update_and_run.sh
+++ update_and_run.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Ustaw prawidłową wartość URL dla CODE_REPO_URL
-CODE_REPO_URL="<url-do-twojego-repozytorium-kodu>"
+# Zdefiniuj zmienne dla repozytoriów
+KLIPY_REPO_DIR="/app"
+TRANSKRYPCJE_REPO_DIR="/RANCZO-TRANSKRYPCJE"
 
-# Pobierz lub zaktualizuj kod aplikacji
-if [ -d "./app" ]; then
-  echo "Aktualizacja istniejącego kodu aplikacji..."
-  cd ./app
-  git pull
+# Aktualizacja repozytorium RANCZO_KLIPY
+cd $KLIPY_REPO_DIR
+git pull
+
+# Klonowanie lub aktualizacja repozytorium RANCZO-TRANSKRYPCJE
+if [ -d "$TRANSKRYPCJE_REPO_DIR" ]; then
+    echo "Aktualizacja repozytorium RANCZO-TRANSKRYPCJE..."
+    cd $TRANSKRYPCJE_REPO_DIR
+    git pull
 else
-  echo "Klonowanie kodu aplikacji..."
-  git clone ${CODE_REPO_URL} ./app
-  cd ./app
+    echo "Klonowanie repozytorium RANCZO-TRANSKRYPCJE..."
+    git clone https://github.com/dam2452/RANCZO-TRANSKRYPCJE.git $TRANSKRYPCJE_REPO_DIR
 fi
 
-# Uruchom aplikację
-python indexer.py
+# Powrót do katalogu z botem
+cd $KLIPY_REPO_DIR
+
+# Uruchom bota
+python bot.py
