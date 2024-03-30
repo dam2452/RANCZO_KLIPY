bf5a75da3447237f2a1efed05efcfb7c4eead15e
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-03-30 21:29:12 +0100

    more

---
+++ DataModify/ModifyJson.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+def load_episode_info():
+    with open('EpisodeInfo.json', 'r', encoding='utf-8') as file:
+        episode_info = json.load(file)
+    return episode_info
+
+def add_episode_info_to_transcriptions(base_path="RANCZO-TRANSKRYPCJE"):
+    episode_info = load_episode_info()
+
+    for season in range(1, 11):  # Dla 10 sezonów
+        for episode in range(1, 14):  # Zakładając maksymalnie 13 odcinków na sezon
+            episode_file_name = f"Ranczo_S{season:02d}E{episode:02d}.json"
+            season_str = str(season)
+            episodes_in_season = episode_info.get(season_str, [])
+
+            if episode <= len(episodes_in_season):
+                episode_info_data = episodes_in_season[episode - 1]
+
+                transcriptions_path = os.path.join(base_path, f"Sezon {season}", episode_file_name)
+                if os.path.exists(transcriptions_path):
+                    with open(transcriptions_path, 'r', encoding='utf-8') as file:
+                        transcriptions = json.load(file)
+
+                    # Dodajemy informacje o sezonie do danych odcinka
+                    episode_info_data_with_season = {
+                        "season": season,  # Dodajemy numer sezonu
+                        **episode_info_data
+                    }
+
+                    # Tworzymy nowy słownik z rozszerzonymi informacjami o odcinku na początku
+                    updated_transcriptions = {
+                        "episode_info": episode_info_data_with_season,
+                        "segments": transcriptions.get("segments", [])
+                    }
+
+                    # Zapisujemy zmodyfikowane transkrypcje z powrotem do pliku
+                    with open(transcriptions_path, 'w', encoding='utf-8') as file:
+                        json.dump(updated_transcriptions, file, ensure_ascii=False, indent=4)
+                    print(f"Updated: {transcriptions_path}")
+                else:
+                    print(f"File not found: {transcriptions_path}")
+            else:
+                print(f"No episode info available for Season {season}, Episode {episode}. Skipping...")
+
+if __name__ == "__main__":
+    add_episode_info_to_transcriptions()
--- indexer.py
+++ indexer.py
@@ -4,6 +4,7 @@ from elasticsearch import Elasticsearch, helpers
 import json
 import os
 from dotenv import load_dotenv
+import re
 
 # Załaduj zmienne środowiskowe z pliku .env
 load_dotenv("passwords.env")
@@ -26,10 +27,35 @@ es = Elasticsearch(
     verify_certs=False,
 )
 
+def load_episode_data():
+    with open('DataModify/EpisodeInfo.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    episode_data = {}
+    for season, episodes in data.items():
+        for episode in episodes:
+            episode_key = f"{season}_{episode['episode_number']}"  # Klucz w formacie "sezon_numerOdcinka"
+            episode_data[episode_key] = {
+                "episode_number": episode['episode_number'],
+                "title": episode['title'],
+                "premiere_date": episode['premiere_date'],
+                "viewership": episode['viewership']
+            }
+    return episode_data
+
+
 def index_transcriptions(base_path="RANCZO-TRANSKRYPCJE"):
+    episode_data = load_episode_data()
     actions = []
     for season_dir in os.listdir(base_path):
         season_path = os.path.join(base_path, season_dir)
+
+        # Zmieniona część: Ekstrakcja numeru sezonu przy użyciu wyrażeń regularnych
+        match = re.search(r'\d+', season_dir)
+        if not match:
+            logger.error(f"Could not extract season number from directory name: {season_dir}")
+            continue  # Przechodzi do następnego katalogu, pomijając obecny
+        season_number = match.group()
+
         if os.path.isdir(season_path):
             for episode_file in os.listdir(season_path):
                 if episode_file.endswith(".json"):
@@ -38,15 +64,30 @@ def index_transcriptions(base_path="RANCZO-TRANSKRYPCJE"):
                     logger.info(f"Indexing transcription from: {file_path}")
                     with open(file_path, 'r', encoding='utf-8') as file:
                         transcription = json.load(file)
-                        for segment in transcription["segments"]:
-                            segment["video_path"] = os.path.join("RANCZO-WIDEO", season_dir, episode_video_path)
-                            actions.append({
-                                "_index": "ranczo-transcriptions",
-                                "_source": segment
-                            })
+                        # Uwzględniamy fakt, że numer odcinka może być częścią nazwy pliku
+                        episode_number_match = re.search(r'S\d+E(\d+)', episode_file)
+                        if episode_number_match:
+                            episode_number = episode_number_match.group(1)
+                            episode_key = f"{season_number}_{episode_number}"
+                            if episode_key in episode_data:
+                                episode_info = episode_data[episode_key]
+                                for segment in transcription["segments"]:
+                                    segment["video_path"] = os.path.join("RANCZO-WIDEO", season_dir,
+                                                                         episode_video_path).replace("\\", "/")
+                                    segment["season"] = int(season_number)
+                                    segment["episode_data"] = episode_info  # Dodanie danych odcinka do segmentu
+                                    actions.append({
+                                        "_index": "ranczo-transcriptions",
+                                        "_source": segment
+                                    })
     logger.info("Bulk indexing transcriptions...")
     helpers.bulk(es, actions)
     logger.info("Transcriptions indexed successfully.")
 
+def delete_index(index_name):
+    if es.indices.exists(index=index_name):
+        es.indices.delete(index=index_name)
+
 if __name__ == "__main__":
+    delete_index("ranczo-transcriptions")
     index_transcriptions()
