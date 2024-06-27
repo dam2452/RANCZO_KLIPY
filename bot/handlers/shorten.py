62919f6253225273826f8d59ddb242ad3e04d576
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-06-27 16:46:27 +0200

    /skroc logi + komunikaty z userem

--- bot/handlers/shorten.py
+++ bot/handlers/shorten.py
@@ -1,4 +1,3 @@
-# shorten.py
 import logging
 import os
 import tempfile
@@ -19,13 +18,15 @@ router = Router()
 async def handle_shorten_request(message: types.Message, bot: Bot):
     try:
         if not await is_user_authorized(message.from_user.username):
-            await message.answer("Nie masz uprawnień do korzystania z tego bota.")
+            await message.answer("❌ Nie masz uprawnień do korzystania z tego bota.")
+            logger.warning(f"Unauthorized access attempt by user: {message.from_user.username}")
             return
 
         chat_id = message.chat.id
         content = message.text.split()
         if len(content) not in (3, 4):
-            await message.answer("Podaj numer klipu (opcjonalnie), sekundy przed i sekundy po.")
+            await message.answer("✂️ Podaj numer klipu (opcjonalnie), sekundy przed i sekundy po. Przykład: /skroc 1 5 5")
+            logger.info("Invalid number of arguments provided by user.")
             return
 
         if len(content) == 4:
@@ -33,37 +34,29 @@ async def handle_shorten_request(message: types.Message, bot: Bot):
             reduce_before = float(content[2])
             reduce_after = float(content[3])
             if chat_id not in last_search_quotes:
-                await message.answer("Najpierw wykonaj wyszukiwanie za pomocą /szukaj.")
+                await message.answer("🔍 Najpierw wykonaj wyszukiwanie za pomocą /szukaj.")
+                logger.info("No previous search results found for user.")
                 return
             segments = last_search_quotes[chat_id]
             segment = segments[index]
         else:
             if chat_id not in last_selected_segment:
-                await message.answer(
-                    "Nie znaleziono żadnego wybranego segmentu. Użyj najpierw komendy /klip lub /wybierz.")
+                await message.answer("❌ Nie znaleziono żadnego wybranego segmentu. Użyj najpierw komendy /klip lub /wybierz.")
+                logger.info("No previously selected segment found for user.")
                 return
             segment = last_selected_segment[chat_id]
             reduce_before = float(content[1])
             reduce_after = float(content[2])
 
-        # Używaj rozszerzonych czasów z zapisanego segmentu
         original_start_time = segment['start'] - EXTEND_BEFORE
         original_end_time = segment['end'] + EXTEND_AFTER
-
-        # Logowanie dla debugowania
-        # logger.info("--------------------------------------------------------------------------------------------")
-        # logger.info(f"Original start time: {original_start_time}, Original end time: {original_end_time}")
-        # logger.info(f"Reduced by {reduce_before} seconds before and {reduce_after} seconds after")
 
         new_start_time = original_start_time + reduce_before
         new_end_time = original_end_time - reduce_after
 
-        # logger.info(f"New start time: {new_start_time}, New end time: {new_end_time}")
-        # logger.info("--------------------------------------------------------------------------------------------")
-
-        # Upewnij się, że nowe czasy mieszczą się w oryginalnym zakresie klipu
         if new_start_time < 0 or new_end_time > original_end_time or new_start_time >= new_end_time:
-            await message.answer("Nie można skrócić klipu, aby jego długość była równa lub mniejsza niż 0 lub poza zakresem oryginalnego klipu.")
+            await message.answer("❌ Nie można skrócić klipu poza zakres oryginalnego klipu lub do długości równej lub mniejszej niż 0.")
+            logger.warning(f"Invalid shorten request: new start time {new_start_time}, new end time {new_end_time}")
             return
 
         video_path = segment['video_path']
@@ -71,14 +64,11 @@ async def handle_shorten_request(message: types.Message, bot: Bot):
         await extract_clip(video_path, new_start_time, new_end_time, output_filename)
 
         input_file = FSInputFile(output_filename)
-        await bot.send_video(message.chat.id, input_file)
-                             #caption=f"Skrócony klip: S{segment['episode_info']['season']}E{segment['episode_info']['episode_number']}")
+        await bot.send_video(message.chat.id, input_file, caption="✂️ Skrócony klip! ✂️")
 
-        # Przechowuj skrócone czasy w segmentu
         segment['expanded_start'] = new_start_time
         segment['expanded_end'] = new_end_time
 
-        # Przechowuj skrócony klip
         with open(output_filename, 'rb') as file:
             video_data = file.read()
 
@@ -86,10 +76,11 @@ async def handle_shorten_request(message: types.Message, bot: Bot):
         last_selected_segment[chat_id]['expanded_clip'] = BytesIO(video_data)
 
         os.remove(output_filename)
+        logger.info(f"Shortened clip for segment {segment['id']} sent to user '{message.from_user.username}'.")
 
     except Exception as e:
-        logger.error(f"Error handling /skroc command: {e}", exc_info=True)
-        await message.answer("Wystąpił błąd podczas przetwarzania żądania.")
+        logger.error(f"Error handling /skroc command for user '{message.from_user.username}': {e}", exc_info=True)
+        await message.answer("⚠️ Wystąpił błąd podczas przetwarzania żądania. Prosimy spróbować ponownie później.")
 
 def register_shorten_command(dispatcher: Dispatcher):
     dispatcher.include_router(router)
