1a565bbf4ca598dd1821d44d8bf2a9a5ae570751
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-07-05 11:08:36 +0200

    Update BOT_MAKE_TRANSCRIPTION.py

--- Preprocessing/BOT_MAKE_TRANSCRIPTION.py
+++ Preprocessing/BOT_MAKE_TRANSCRIPTION.py
@@ -9,7 +9,7 @@ def przetworz_folder(folder_wejsciowy, folder_wyjsciowy):
 
     for sciezka_katalogu, _, lista_plikow in os.walk(folder_wejsciowy):
         for plik in lista_plikow:
-            if plik.endswith('.wav' or '.mp3'):
+            if plik.endswith(('.wav', '.mp3')):  # Poprawione sprawdzanie rozszerzeń
                 sciezka_wejsciowa = os.path.join(sciezka_katalogu, plik)
                 nazwa_pliku_wyjsciowego = plik.split('.')[0] + '_przetworzone.wav'
                 sciezka_wyjsciowa = os.path.join(folder_wyjsciowy, os.path.relpath(sciezka_katalogu, folder_wejsciowy), nazwa_pliku_wyjsciowego)
@@ -21,7 +21,7 @@ def przetworz_folder(folder_wejsciowy, folder_wyjsciowy):
                 subprocess.run(["whisper", sciezka_wejsciowa, "--model", model, "--language", jezyk, "--device", urzadzenie, "--output_dir", folder_wyjsciowy])
 
 def main():
-    parser = argparse.ArgumentParser(description='Przetwarzanie plików .wav za pomocą modelu whisper')
+    parser = argparse.ArgumentParser(description='Przetwarzanie plików .wav i .mp3 za pomocą modelu whisper')
     parser.add_argument('folder_wejsciowy', type=str, help='Ścieżka do folderu wejściowego')
     parser.add_argument('folder_wyjsciowy', type=str, help='Ścieżka do folderu wyjściowego')
 
