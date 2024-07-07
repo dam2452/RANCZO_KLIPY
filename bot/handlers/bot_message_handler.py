55c7dedce15e3722ebf98f06a9f7041c01859625
skelly37 <skelly37@protonmail.com>
2024-07-07 12:11:01 +0200

    done for today

--- bot/handlers/bot_message_handler.py
+++ bot/handlers/bot_message_handler.py
@@ -2,21 +2,51 @@ from abc import (
     ABC,
     abstractmethod,
 )
-from typing import List
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    List,
+    Optional,
+)
 
 from aiogram import (
+    BaseMiddleware,
     Bot,
     Dispatcher,
 )
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    TelegramObject,
+)
+
+
+class DummyMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Optional[Awaitable]:
+        raise Exception("tbd: zmienic na BotMiddleware, wyniesc do folderu odpowiedniego z tego pliku i uzyc jako baza obecnych")
 
 
 class BotMessageHandler(ABC):
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: Bot, middlewares: Optional[List[BaseMiddleware]] = None) -> None:
         self._bot = bot
 
+        self._middlewares = middlewares if middlewares else [
+            DummyMiddleware(),
+            DummyMiddleware(),
+            # todo
+        ]
+
     def register(self, dp: Dispatcher) -> None:
+        for middleware in self._middlewares:
+            dp.message.middleware(middleware)
+
         dp.message.register(self.handle, Command(commands=self.get_commands()))
 
     @abstractmethod
