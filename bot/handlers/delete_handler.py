7a85e973fa5d4e224d0196c02a003c4044090cdd
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-07-10 19:47:02 +0200

    wip

---
+++ bot/handlers/delete_handler.py
@@ -0,0 +1,46 @@
+import logging
+from typing import List
+
+from aiogram.types import Message
+
+from bot_message_handler import BotMessageHandler
+from bot.utils.database import DatabaseManager
+
+
+class DeleteClipHandler(BotMessageHandler):
+    def get_commands(self) -> List[str]:
+        return ['usunklip', 'deleteclip', 'uk']
+
+    def get_action_name(self) -> str:
+        return "delete_clip_handler"
+
+    async def _do_handle(self, message: Message) -> None:
+        await self._log_user_activity(message.from_user.username, f"/usunklip {message.text}")
+        username = message.from_user.username
+
+        command_parts = message.text.split(maxsplit=1)
+        if len(command_parts) < 2:
+            await self.__reply_no_clip_name_provided(message)
+            return
+
+        clip_name = command_parts[1]
+
+        result = await DatabaseManager.delete_clip(username, clip_name)
+
+        if result == "DELETE 0":
+            await self.__reply_clip_not_exist(message, clip_name, username)
+        else:
+            await self.__reply_clip_deleted(message, clip_name, username)
+
+    async def __reply_no_clip_name_provided(self, message: Message) -> None:
+        await message.answer("❌ Podaj nazwę klipu do usunięcia. Przykład: /usunklip nazwa_klipu ❌")
+        await self._log_system_message(logging.INFO, "No clip name provided by user.")
+
+    async def __reply_clip_not_exist(self, message: Message, clip_name: str, username: str) -> None:
+        await message.answer(f"🚫 Klip o nazwie '{clip_name}' nie istnieje.🚫")
+        await self._log_system_message(logging.INFO, f"Clip '{clip_name}' does not exist for user '{username}'.")
+
+    async def __reply_clip_deleted(self, message: Message, clip_name: str, username: str) -> None:
+        await message.answer(f"✅ Klip o nazwie '{clip_name}' został usunięty.✅")
+        await self._log_system_message(logging.INFO,
+                                       f"Clip '{clip_name}' has been successfully deleted for user '{username}'.")
