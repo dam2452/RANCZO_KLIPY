b2818ae6fe24f8d73ca0da7aab8a5b1c3060ed44
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-07-10 20:01:14 +0200

    wip

---
+++ bot/handlers/compile_saved_handler.py
@@ -0,0 +1,124 @@
+import json
+import logging
+import os
+import tempfile
+from typing import (
+    List,
+    Dict,
+)
+
+from bot.main import bot
+
+from aiogram.types import (
+    Message,
+    FSInputFile,
+)
+
+from bot_message_handler import BotMessageHandler
+
+from bot.utils.database import DatabaseManager
+from bot.utils.video_handler import VideoManager
+
+last_compiled_clip: Dict[int, json] = {}
+
+
+async def compile_clips(selected_clips_data) -> str: #fixme to chyba też jakiś wylot do utils
+    temp_files = []
+    for video_data in selected_clips_data:
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
+        temp_files.append(temp_file.name)
+        with open(temp_file.name, 'wb') as f:
+            f.write(video_data)
+
+    compiled_output = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
+    compiled_output.close()
+
+    video_manager = VideoManager(bot)
+    await video_manager.concatenate_clips(temp_files, compiled_output.name)
+
+    file_size_mb = os.path.getsize(compiled_output.name) / (1024 * 1024)
+    if file_size_mb > 50:
+        raise ValueError(f"Compiled clip exceeds size limit: {file_size_mb:.2f} MB")
+
+    return compiled_output.name
+
+
+async def send_compiled_clip(message: Message, chat_id: int, compiled_output: str) -> None:
+    with open(compiled_output, 'rb') as f:
+        compiled_clip_data = f.read()
+
+    last_compiled_clip[chat_id] = {
+        'compiled_clip': compiled_clip_data,
+        'is_compilation': True,
+    }
+
+    await bot.send_video(chat_id, FSInputFile(compiled_output), supports_streaming=True, width=1920, height=1080)
+
+
+async def clean_up_temp_files(compiled_output: str, selected_clips_data) -> None:
+    for temp_file in selected_clips_data:
+        os.remove(temp_file)
+    os.remove(compiled_output)
+
+
+class CompileSelectedClipsHandler(BotMessageHandler):
+    def get_commands(self) -> List[str]:
+        return ['polaczklipy', 'concatclips', 'pk']
+
+    def get_action_name(self) -> str:
+        return "compile_saved_handler"
+
+    async def _do_handle(self, message: Message) -> None:
+        await self._log_user_activity(message.from_user.username, f"/polaczklipy {message.text}")
+        username = message.from_user.username
+
+        chat_id = message.chat.id
+        content = message.text.split()
+
+        if len(content) < 2:
+            await self.__reply_no_clip_names_provided(message)
+            return
+
+        clip_names = content[1:]
+        selected_clips_data = []
+
+        for clip_name in clip_names:
+            clip = await DatabaseManager.get_clip_by_name(username, clip_name)
+            if not clip:
+                await self.__reply_clip_not_found(message, clip_name, username)
+                return
+            selected_clips_data.append(clip[0])
+
+        if not selected_clips_data:
+            await self.__reply_no_matching_clips_found(message)
+            return
+
+        try:
+            compiled_output = await compile_clips(selected_clips_data)
+            await send_compiled_clip(message, chat_id, compiled_output)
+            await clean_up_temp_files(compiled_output, selected_clips_data)
+
+            await self._log_system_message(logging.INFO,
+                                           f"Compiled clip sent to user '{username}' and temporary files removed.")
+        except Exception as e:
+            await self.__reply_compilation_error(message, e)
+
+    async def __reply_user_not_authorized(self, message: Message) -> None:
+        await message.answer("❌ Nie masz uprawnień do korzystania z tego bota.❌")
+        await self._log_system_message(logging.WARNING, "Unauthorized access attempt by user.")
+
+    async def __reply_no_clip_names_provided(self, message: Message) -> None:
+        await message.answer("📄 Podaj nazwy klipów do skompilowania w odpowiedniej kolejności.")
+        await self._log_system_message(logging.INFO, "No clip names provided by user.")
+
+    async def __reply_clip_not_found(self, message: Message, clip_name: str, username: str) -> None:
+        await message.answer(f"❌ Nie znaleziono klipu o nazwie '{clip_name}'.")
+        await self._log_system_message(logging.INFO, f"Clip '{clip_name}' not found for user '{username}'.")
+
+    async def __reply_no_matching_clips_found(self, message: Message) -> None:
+        await message.answer("❌ Nie znaleziono pasujących klipów do kompilacji.")
+        await self._log_system_message(logging.INFO, "No matching clips found for compilation.")
+
+    async def __reply_compilation_error(self, message: Message, exception: Exception) -> None:
+        await message.answer("⚠️ Wystąpił błąd podczas kompilacji klipów.⚠️")
+        await self._log_system_message(logging.ERROR, f"An error occurred while compiling clips: {exception}")
