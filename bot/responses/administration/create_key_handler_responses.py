1628d4067c97ab2bf5b9f44102a9d58b94410bde
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-08-28 15:34:35 +0200

    fix

--- bot/responses/administration/create_key_handler_responses.py
+++ bot/responses/administration/create_key_handler_responses.py
@@ -1,6 +1,6 @@
 
 def get_create_key_usage_message() -> str:
-    return "❌ Podaj liczbę dni i notatkę. Przykład: /addkey 30 Promocja"
+    return "❌ Podaj liczbę dni i klucz. Przykład: /addkey 30 tajny_klucz ❌"
 
 
 def get_create_key_success_message(days: int, key: str) -> str:
