f38da6b2e5e2f46aea1af595bb71ccf31b5fe3f3
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-10-24 17:10:22 +0200

    Create test_admin_help.py

---
+++ bot/tests/e2e_tests/administration/test_admin_help.py
@@ -0,0 +1,43 @@
+from bot.tests.e2e_tests.base_test import BaseE2ETest
+
+
+class TestAdminCommand(BaseE2ETest):
+    def test_admin(self):
+        test_cases = [
+            {
+                'command': ['/admin'],
+                'expected_fragments': [
+                    "/addwhitelist",
+                    "/removewhitelist",
+                    "/listwhitelist",
+                    "/listadmins",
+                    "/listmoderators",
+                    "/klucz",
+                    "/listkey",
+                    "/addkey",
+                    "/removekey",
+                    "/addsubscription",
+                    "/removesubscription",
+                    "/transkrypcja"
+                ],
+            },
+            {
+                'command': ['/admin skroty'],
+                'expected_fragments': [
+                    "/addw",
+                    "/rmw",
+                    "/lw",
+                    "/la",
+                    "/lm",
+                    "/klucz",
+                    "/lk",
+                    "/addk",
+                    "/rmk",
+                    "/addsub",
+                    "/rmsub",
+                    "/t"
+                ],
+            },
+        ]
+
+        self.run_test_cases(test_cases)
