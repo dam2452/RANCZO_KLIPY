2194b25d7fd7ae268da7ff06cb947f47d6f0378e
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-11-28 21:21:16 +0100

    duplicate code fix

--- bot/tests/settings.py
+++ bot/tests/settings.py
@@ -1,48 +1,35 @@
 import logging
-import os
-from pathlib import Path
 
-from dotenv import load_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
-# pylint: disable=duplicate-code
+from bot.utils.config_loader import load_env_file
+
 logger = logging.getLogger(__name__)
-
-env_file = os.getenv('ENV_FILE')
-if env_file:
-    env_path = Path(env_file)
-else:
-    env_path = Path(__file__).parent.parent / ".env"
-
-if env_path.exists():
-    load_dotenv(env_path)
-    logger.warning("Using dotenv file")
-else:
-    logger.info("No dotenv file found. Environment variables should be provided by the system.")
-# pylint: enable=duplicate-code
+env_path = load_env_file()
 
 class Settings(BaseSettings):
-
     SESSION: str = Field("name")
-    API_ID: int = Field()
-    API_HASH: str = Field()
-    BOT_USERNAME: str = Field()
-    PASSWORD: str = Field()
-    PHONE: str = Field()
+    API_ID: int = Field(...)
+    API_HASH: str = Field(...)
+    BOT_USERNAME: str = Field(...)
+    PASSWORD: str = Field(...)
+    PHONE: str = Field(...)
 
-    TEST_POSTGRES_DB: str = Field()
-    TEST_POSTGRES_USER: str = Field()
-    TEST_POSTGRES_PASSWORD: str = Field()
-    TEST_POSTGRES_HOST: str = Field()
-    TEST_POSTGRES_PORT: str = Field()
+    TEST_POSTGRES_DB: str = Field(...)
+    TEST_POSTGRES_USER: str = Field(...)
+    TEST_POSTGRES_PASSWORD: str = Field(...)
+    TEST_POSTGRES_HOST: str = Field(...)
+    TEST_POSTGRES_PORT: str = Field(...)
 
     DEFAULT_ADMIN: int = Field(...)
     ADMIN_USERNAME: str = Field("Admin")
     ADMIN_FULL_NAME: str = Field("Admin")
+
     class Config:
-        env_file = env_path
+        env_file = str(env_path)
         env_prefix = ""
         extra = "ignore"
 
+logger.info(f"Loading settings from {Settings.Config.env_file}")
 settings = Settings()
