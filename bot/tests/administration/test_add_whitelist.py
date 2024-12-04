a9ffad158a070e4077b0de511ea1b5f088cb28ca
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-12-04 15:03:43 +0100

    Update test_add_whitelist.py

--- bot/tests/administration/test_add_whitelist.py
+++ bot/tests/administration/test_add_whitelist.py
@@ -1,6 +1,9 @@
 import pytest
 
+from bot.database.database_manager import DatabaseManager
 from bot.tests.base_test import BaseTest
+# import bot.responses.administration.add_whitelist_handler_responses as add_msg
+# import bot.responses.administration.remove_whitelist_handler_responses as remove_msg
 
 
 @pytest.mark.usefixtures("db_pool", "telegram_client")
@@ -26,7 +29,7 @@ class TestWhitelistCommands(BaseTest):
     #         f'/removewhitelist {user_id}',
     #         [remove_msg.get_user_removed_message(str(user_id))],
     #     )
-
+    #
     # @pytest.mark.quick
     # @pytest.mark.asyncio
     # async def test_add_nonexistent_user_whitelist(self):
@@ -35,7 +38,7 @@ class TestWhitelistCommands(BaseTest):
     #         f'/addwhitelist {user_id}',
     #         [add_msg.get_user_not_found_message()],
     #     )
-
+    #
     # @pytest.mark.asyncio
     # async def test_add_whitelist_invalid_user_id_format(self):
     #     user_id_invalid = "invalid_id"
@@ -43,7 +46,7 @@ class TestWhitelistCommands(BaseTest):
     #         f'/addwhitelist {user_id_invalid}',
     #         [add_msg.get_no_user_id_provided_message()],
     #     )
-
+    #
     # @pytest.mark.asyncio
     # async def test_add_user_with_no_username(self):
     #     user_id = 888888888
