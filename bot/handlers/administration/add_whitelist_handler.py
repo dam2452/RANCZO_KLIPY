551551a82d85b25ea3c8062844eeb4d866311951
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-12-21 18:56:32 +0100

    fix

--- bot/handlers/administration/add_whitelist_handler.py
+++ bot/handlers/administration/add_whitelist_handler.py
@@ -1,7 +1,6 @@
 import logging
 from typing import List
 
-from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import Message
 
 from bot.database.database_manager import DatabaseManager
@@ -11,9 +10,9 @@ from bot.handlers.bot_message_handler import (
 )
 from bot.responses.administration.add_whitelist_handler_responses import (
     get_log_user_added_message,
+    get_no_user_id_provided_message,
     get_no_username_provided_message,
     get_user_added_message,
-    get_user_not_found_message,
 )
 
 
@@ -33,24 +32,26 @@ class AddWhitelistHandler(BotMessageHandler):
 
     async def _do_handle(self, message: Message) -> None:
         user_input = message.text.split()[1]
-        try:
-            user_data = await self._bot.get_chat(int(user_input) if user_input.isdigit() else user_input)
-        except TelegramBadRequest as e:
-            if "chat not found" in str(e).lower():
-                await self._answer(message, get_user_not_found_message())
-                return
-            raise
+
+        if not user_input.isdigit():
+            await self.__reply_user_not_found(message)
+            return
 
         await DatabaseManager.add_user(
-            user_id=user_data.id,
-            username=user_data.username,
-            full_name=user_data.full_name,
+            user_id=int(user_input),
+            username="",
+            full_name="",
             note=None,
         )
-
-        username_or_name = user_data.username or user_data.full_name or str(user_data.id)
-        await self.__reply_user_added(message, username_or_name)
+        await self.__reply_user_added(message, user_input)
 
     async def __reply_user_added(self, message: Message, username: str) -> None:
-        await self._answer(message,get_user_added_message(username))
-        await self._log_system_message(logging.INFO, get_log_user_added_message(username, message.from_user.username))
+        await self._answer(message, get_user_added_message(username))
+        await self._log_system_message(
+            logging.INFO,
+            get_log_user_added_message(username, message.from_user.username),
+        )
+
+    async def __reply_user_not_found(self, message: Message) -> None:
+        await self._answer(message, get_no_user_id_provided_message())
+        await self._log_system_message(logging.INFO, get_no_user_id_provided_message())
