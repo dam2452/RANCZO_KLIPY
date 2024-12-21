943043418798263a5242f09f084d68d61ac4293d
dam2452 <81230036+dam2452@users.noreply.github.com>
2024-12-21 19:21:52 +0100

    fix

--- bot/handlers/administration/create_key_handler.py
+++ bot/handlers/administration/create_key_handler.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 
 from aiogram.types import Message
@@ -10,6 +11,7 @@ from bot.handlers.bot_message_handler import (
 from bot.responses.administration.create_key_handler_responses import (
     get_create_key_success_message,
     get_create_key_usage_message,
+get_key_already_exists_message
 )
 
 
@@ -28,8 +30,33 @@ class CreateKeyHandler(BotMessageHandler):
         )
     async def _do_handle(self, message: Message) -> None:
         args = message.text.split()
-        days = int(args[1])
-        name = " ".join(args[2:])
+        if not args[1].isdigit():
+            await self.__replay_wrong_argument(message)
+            return
 
-        await DatabaseManager.create_subscription_key(days, name)
-        await self._answer(message,get_create_key_success_message(days, name))
+
+        days = int(args[1])
+        key = " ".join(args[2:])
+
+        key_exists = await DatabaseManager.get_subscription_days_by_key(key)
+        if key_exists is not None:
+            await self.__replay_key_already_exists(message, key)
+            return
+
+        await DatabaseManager.create_subscription_key(days, key)
+        await self.__replay_key_added(message, days, key)
+
+    async def __replay_key_added(self, message: Message, days: int, key: str) -> None:
+        await self._answer(message, get_create_key_success_message(days, key))
+
+        await self._log_system_message(
+            logging.INFO, get_create_key_success_message(days, key)
+        )
+
+    async def __replay_wrong_argument(self, message: Message) -> None:
+        await self._answer(message, get_create_key_usage_message())
+        await self._log_system_message(logging.INFO, get_create_key_usage_message())
+
+    async def __replay_key_already_exists(self, message: Message, key: str) -> None:
+        await self._answer(message, get_key_already_exists_message(key))
+        await self._log_system_message(logging.INFO, f"Key already exists: {key}")
