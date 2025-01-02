f58d81c4c4ac0f51f522ee1de63240bd58e80c4e
dam2452 <81230036+dam2452@users.noreply.github.com>
2025-01-02 19:09:20 +0100

    Update delete_clip_handler_responses.py

--- bot/responses/not_sending_videos/delete_clip_handler_responses.py
+++ bot/responses/not_sending_videos/delete_clip_handler_responses.py
@@ -1,15 +1,3 @@
-# def get_invalid_args_count_message() -> str:
-#     return "❌ Podaj numer klipu do usunięcia. Przykład: /usunklip numer_klipu ❌"
-#
-#
-# def get_clip_not_exist_message(clip_number: int) -> str:
-#     return f"🚫 Klip o numerze '{clip_number}' nie istnieje.🚫"
-#
-#
-# def get_clip_deleted_message(clip_name: str) -> str:
-#     return f"✅ Klip o nazwie '{clip_name}' został usunięty.✅"
-
-
 def get_log_clip_not_exist_message(clip_number: int, username: str) -> str:
     return f"Clip '{clip_number}' does not exist for user '{username}'."
 
